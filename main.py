2793a06e65e7c9b75cd083cda3025de19f9ea402
Tanya Kudaisya <134828290+TanyaKudaisya@users.noreply.github.com>
2026-02-13 00:59:38 +0530

    allow the user to provide the limit and offset

--- main.py
+++ main.py
@@ -95,32 +95,26 @@ class PaginatedResponse(BaseModel,Generic[T]):
      data:T
      next:Optional[str]
      prev:Optional[str]
-     count:int
+    #  count:int
 
 # square brackets show type all in here, before we were using ist of type campaign
 
 @app.get("/campaigns", response_model=PaginatedResponse[list[Campaign]])
-async def read_campaigns(request: Request, session: SessionDependency, page:int = Query(1, ge=1), page_size: int =Query(20, ge=1)):#session dependency has to be passed to enable the access of db
-    print(page)
-    limit = page_size
-    offset = (page-1)*limit
+async def read_campaigns(request: Request, session: SessionDependency, offset:int = Query(0, ge=0), limit: int =Query(20, ge=1)):#session dependency has to be passed to enable the access of db
+    
     data = session.exec(select(Campaign).order_by(Campaign.campaign_id).offset(offset).limit(limit)).all()
 
-    base_url = str(request.url).split('?')
-
-    total = session.exec(select(func.count()).select_from(Campaign)).one()
-    #see now this query is basically touching all the rows, might make us slow, thus we can take another approach i.e., completely getting rid of total and letting the query run like this, and depending on the db, it might take us to an empty page for next_url but cool
-    if offset + limit < total:
-        next_url = f"{base_url}?page={page+1}&page_size={limit}"
-    else:
-        next_url = None
+    base_url = str(request.url).split('?')[0]
     
-    if page>1:
-        prev_url = f"{base_url}?page={page-1}&page_size={limit}"
+    next_url = f"{base_url}?offset={offset+limit}&limit={limit}"
+    
+    
+    if offset>1:
+        prev_url = f"{base_url}?offset={max(max(0,offset-limit))}&limit={limit}"
     else:
         prev_url=None
     return {
-        "count":total,
+        # "count":total,
         "next":next_url,
         "prev":prev_url,
         "data": data
